3d1901a59db00f0b381ce733c4ff2461e2526a10
tripinblindkids <dcj25@njit.edu>
2024-11-13 12:51:23 -0500

    a
    yaya

---
+++ Lab_8.js
@@ -0,0 +1,88 @@
+
+
+let name = "Dylan"; 
+let age = 18; 
+console.log("Name:", name);
+console.log("Age:", age);
+
+
+let str = "Hello";
+let num = 42;
+let bool = true;
+let undef;
+console.log("String:", str);
+console.log("Number:", num);
+console.log("Boolean:", bool);
+console.log("Undefined:", undef);
+
+
+let RollNumber = 15;
+if (RollNumber > 10) {
+    console.log("The RollNumber is greater than 10");
+}
+
+
+let isMember = true;
+if (isMember) {
+    console.log("Member discount applied.");
+}
+
+
+let startValue = 50;
+if (startValue > 0) {
+    console.log("The startValue is positive.");
+} else if (startValue < 0) {
+    console.log("The startValue is negative.");
+} else {
+    console.log("The startValue is zero.");
+}
+
+
+let a = 10;
+let b = 20;
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+
+let firstName = "Dylan";
+let lastName = "Jefferson";
+age = 30;
+console.log(`Hello, ${firstName} ${lastName}, you are ${age} years old.`);
+
+
+num = 50;
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+}
+
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} else {
+    console.log("Check your driving eligibility.");
+}
+
+
+num = 21;
+if (num % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+
+
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 80 && score <= 89) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
